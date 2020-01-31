1f6157974bd34d35a6f7ebd2a34dbbb026a54208
Kamzoki <kaittah@ymail.com>
2020-01-31 21:32:59 +0200

    Character movement

---
+++ Assets/Scripts/UIButtons.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIButtons : MonoBehaviour
+{
+    [SerializeField]
+    Vector2 direction;
+
+    bool isMoving = false;
+
+    public void JumpBtn()
+    {
+        if (GetPlayer())
+        {
+            GetPlayer().GetComponent<PlayerMovement>().JumpPlayer();
+        }
+    }
+
+    public void MoveBtn()
+    {
+        if (GetPlayer())
+        {
+            isMoving = true;
+        }
+    }
+
+    public void MoveBtnExit()
+    {
+        isMoving = false;
+    }
+
+
+    private GameObject GetPlayer()
+    {
+        return GameObject.FindGameObjectWithTag("Player");
+    }
+
+    private void Update()
+    {
+        if (isMoving)
+        {
+            GetPlayer().GetComponent<PlayerMovement>().MovePlayer(direction);
+        }
+    }
+}
